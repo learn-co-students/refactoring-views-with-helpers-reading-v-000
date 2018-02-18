5e1f6c419fb10012fd4c78be1556584d4d0f7766
Scott Wakeman <github email address>
2018-02-18 12:15:50 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'authors/show'
-
   get 'test/index'
-
+  get '/authors/:id', to: 'authors#show'
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
+
+
 end
