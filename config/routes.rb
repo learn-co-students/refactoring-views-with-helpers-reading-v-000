baaf9df7f56c4e2e2cc7e534e2be804b7f5558ce
Brittany Morris <brittmmendez@gmail.com>
2018-02-22 17:14:42 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'authors/show'
+  resources :authors, only: [:show]
 
-  get 'test/index'
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
