608edc5f35ae35b5ce0ac7f0a5328c6932a4a7bd
daniellecroxton <danikrist@gmail.com>
2018-02-22 17:26:14 -0700

    Done.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
+describe 'create' do
+  before do
+    @author = Author.create(name: "Hemingway")
+  end
+
+
   describe "GET #show" do
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
   end
-
+end
 end
