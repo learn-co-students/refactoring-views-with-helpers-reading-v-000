184fb8f0c2146f9b1bb43c042cac2dcc1dd8d28b
independnt <gonzalezdennis6@gmail.com>
2018-03-20 13:03:19 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+post = Post.create(title: "The best post", description: "It's not very interesting though")
