ada2bb271c03d29b76611991f694a23be3520ace
Alli Tzadok <allisontzadok@gmail.com>
2018-04-11 14:31:09 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params)
+	  @post = Post.new(title: params[:title], description: params[:description])
 	  @post.save
 	  redirect_to post_path(@post)
 	end
