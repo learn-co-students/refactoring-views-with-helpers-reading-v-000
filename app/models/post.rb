0a74666dfe15c757eb62bb6e7707cf1fb19423cc
Shmully Wolfson <shmullywolfson@gmail.com>
2018-04-18 15:31:48 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,13 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case
   belongs_to :author
 
+  def last_updated
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
+
   private
+
+
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
