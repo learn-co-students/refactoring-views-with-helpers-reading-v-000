0ba87c4afa6273ec7f9726695f47a62b10d7abd6
Jayon Kim <jpkim921@gmail.com>
2018-08-10 20:06:46 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+author = Author.create(name: "name", genre: "genre", bio: "bio")
