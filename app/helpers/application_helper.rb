4cd854912fc62f4e2485149321902a1a59895190
pracniscate <lorena.giddens@outlook.com>
2018-10-02 22:41:08 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    # dynamically change the title of each page based on what the page is about
+    def title(text)
+        content_for :title, text
+    end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+    def last_updated(post)
+        post.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+        # Tuesday, Oct 02, at 10:19
+    end
 end
