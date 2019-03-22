f3f15e18db0ab3336dbd694984645f3fb31b9566
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-22 16:45:07 +0000

    Done.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
+if RUBY_VERSION >= '2.6.0'
+ if Rails.version < '5'
+   class ActionController::TestResponse < ActionDispatch::TestResponse
+     def recycle!
+       # hack to avoid MonitorMixin double-initialize error:
+       @mon_mutex_owner_object_id = nil
+       @mon_mutex = nil
+       initialize
+     end
+   end
+ else
+   puts "Monkeypatch for ActionController::TestResponse no longer needed"
+ end
+end
 
 RSpec.describe AuthorsController, type: :controller do
 
@@ -9,5 +23,4 @@ RSpec.describe AuthorsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
