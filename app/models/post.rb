4a4b0b0ddf0ab0cc69884464b4e09a432b9a36b9
github username <github email address>
2019-05-22 20:01:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,10 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def last_updated
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
+
   def make_title_case
     self.title = self.title.titlecase
   end
