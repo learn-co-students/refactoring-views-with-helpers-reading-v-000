d0cbe760eb0f75e85a0e96d56abfdb89e85e8fa1
Kat Tochowicz <ktochow1@binghamton.edu>
2019-08-12 19:02:52 -0400

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def title(text)
+    content_for :title, text
+    #sends our text to the place in the application layout
+    #that is expecting some content for :title
+  end
 end
