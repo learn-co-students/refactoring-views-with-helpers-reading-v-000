266c5ab94864b68a287d69bfb339c53263a0f02f
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-25 18:16:54 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # This helper is created automatically with your Rails project and is where you keep helpers that are related to the application itself rather than any given model or controller.
+
+  def title(text)
+    # What this will do is send our text to the place in our application layout that is expecting some content for the :title.
+    content_for :title, text
+  end
 end
