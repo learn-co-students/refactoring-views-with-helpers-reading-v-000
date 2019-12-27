390619fae18823d92e8a8180656d3ecaa04c2bbc
sdcro <sdcrouse91@gmail.com>
2019-12-26 18:35:00 -0800

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def title(text)
-    content_for :title, text
+    # if text
+      content_for :title, text
+    # else
+      # content_for :title, "BlogFlash"
+    # end
   end
 end
