c92d05def3b71a2d099ef270171b1bc51c126158
sdcro <sdcrouse91@gmail.com>
2019-12-27 14:26:07 -0800

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       # content_for :title, "BlogFlash"
     # end
   end
+
+  def display_title
+    # content_for?(:title) ? content_for(:title) : "BlogFlash"
+    # For this to work, I can't call the #title helper with title("BlogFlash").
+
+    content_for(:title) || "BlogFlash" # This works, too! 
+    # I can also just put this line ^^^ inside of the ERB tag in the <title>.
+  end
 end
