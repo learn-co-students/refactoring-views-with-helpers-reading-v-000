8e73207941e3759add36b9a4f1c3b4ad4f977a15
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-16 17:05:19 +0000

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
-def title(text)
-  content_for :title, text #sends text to the place in the app layout that is expecting some contetnt for the :title
+module ApplicationHelper
+  def title(text)
+    content_for :title, text #sends text to the place in the app layout that is expecting some contetnt for the :title
+  end
+
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,5 @@
 module PostsHelper
-end
-
-def last_updated(post)
-  post.updated_at.strftime("Last updated %A, %b %e, at %1:%M %p")
+  def last_updated(post)
+    post.updated_at.strftime("Last updated %A, %b %e, at %1:%M %p")
+  end
 end
